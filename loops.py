40b27a23076e23754610f6dd49f6e4218a838738
Abhishek Jha <abhishek.jha.uk@gmail.com>
2022-10-12 14:57:49 +0100

    loops

---
+++ loops.py
@@ -0,0 +1,16 @@
+list_data = [1, 2, 3, 4, 5]
+for number in list_data:
+    if number == 3:
+        print("Found 3")
+    elif number > 3:
+        print("You have passed 3")
+    else:
+        print("too soon")
+
+"""
+create a dictionary
+itreate through the dict
+using control flow - if elif else and for loop print all the keys
+print all the value
+print key with matching values
+"""
---
+++ movie_rating.py
@@ -0,0 +1,8 @@
+user_age = input("Enter your age: ")
+
+age = int(user_age)
+if age < 8:
+    print("You are " + user_age + " year Old. You can only watch \"U\" rated movies")
+
+print(user_age)
+
