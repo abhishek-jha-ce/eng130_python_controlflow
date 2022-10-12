751bef1facf17037ea911d202701c5957945af55
Abhishek Jha <abhishek.jha.uk@gmail.com>
2022-10-12 16:01:26 +0100

    loops

--- loops.py
+++ loops.py
@@ -1,11 +1,11 @@
-list_data = [1, 2, 3, 4, 5]
-for number in list_data:
-    if number == 3:
-        print("Found 3")
-    elif number > 3:
-        print("You have passed 3")
-    else:
-        print("too soon")
+# list_data = [1, 2, 3, 4, 5]
+# for number in list_data:
+#     if number == 3:
+#         print("Found 3")
+#     elif number > 3:
+#         print("You have passed 3")
+#     else:
+#         print("too soon")
 
 """
 create a dictionary
@@ -14,3 +14,33 @@ using control flow - if elif else and for loop print all the keys
 print all the value
 print key with matching values
 """
+
+# Dictionary
+student = {
+    "first_name": "Abhishek",
+    "last_name": "Jha",
+    "Stream": "DevOps",
+    "Location": "London",
+    "Salary": 1000
+}
+
+# Keys in Dictionary
+print("Printing the keys in Dict:")
+print(student.keys())
+
+print("Printing the values in Dict:")
+print(student.values())
+
+# Using Loops
+print("Printing the keys in Dict:")
+for key in student:
+    print(key)
+
+print("Printing the values in Dict:")
+for value in student.values():
+    print(value)
+
+
+print("Printing Keys and Values using loop")
+for key, value in student.items():
+    print(key, ':', value)
